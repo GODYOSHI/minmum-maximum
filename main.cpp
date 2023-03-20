3904b5d7bb422cedfd698799dbe2c18bceeb3b70
GODYOSHI <98141438+GODYOSHI@users.noreply.github.com>
2023-03-20 02:38:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num1;
+  int num2;
+
+cout<< "Enter a number please: ";
+cin >> num1;
+
+
+cout<< "Enter another number please: ";
+cin >> num2;
+
+ if (num1 > num2)
+{
+  cout << num1; 
+} 
+else (num1 < num2);
+  
+{
+  cout << "The larger number is ";
+  cout << num2;
+} 
+
+  
+  
+
+
+
+
+  
+}
